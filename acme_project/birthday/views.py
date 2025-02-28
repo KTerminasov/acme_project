e70a79da819aed2e681adc2ca1d6898be48fa596
Кирилл Терминасов <kterminasov@gmail.com>
2025-02-28 10:46:02 +0300

    Add data validation

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -3,6 +3,6 @@ from .forms import BirthdayForm
 
 
 def birthday(request):
-    form = BirthdayForm()
+    form = BirthdayForm(request.GET or None)
     context = {'form': form}
     return render(request, 'birthday/birthday.html', context=context)
