4655328ff0f120557c45c5c4bc144672cc681fbf
Кирилл Терминасов <kterminasov@gmail.com>
2025-03-08 22:52:32 +0300

    Replace homepage view-function with CBV

--- acme_project/pages/views.py
+++ acme_project/pages/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+# Импортируем класс TemplateView для наследования.
+from django.views.generic import TemplateView
+# Импортируем модель Birthday
+from birthday.models import Birthday
 
 
-def homepage(request):
-    return render(request, 'pages/index.html')
+class HomePage(TemplateView):
+    # В атрибуте template_name обязательно указывается имя шаблона,
+    # на основе которого будет создана возвращаемая страница.
+    template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста с помощью родительского метода.
+        context = super().get_context_data(**kwargs)
+
+        # Добавляем в словарь ключ total_counr;
+        # значение ключа - число объектов модели Birthday.
+        context['total_count'] = Birthday.objects.count()
+
+        # Возвращаем измененный словарь контекста.
+        return context
+
